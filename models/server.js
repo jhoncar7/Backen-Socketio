d697804ab8b4c2408f60f2cf0c66f174a064c63f
JhonCar <jhon7car@gmail.com>
2022-04-26 16:34:54 -0300

    first version

---
+++ models/server.js
@@ -0,0 +1,58 @@
+//servidor de express
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+
+        //http server
+        this.server = http.createServer(this.app);
+
+        //configuracion del socket server
+        this.io = socketio(this.server, {/*Configuraciones*/ });
+    }
+
+    middleware() {
+        //desplegar directorio publico
+        //app.use(express.static('public'));
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+    }
+
+    configurarSocket() {
+        new Sockets(this.io);
+    }
+
+    execute() {
+        this.middleware();
+        this.configurarSocket();
+        this.server.listen(this.port, () => {
+            console.log('servidor corriendo en el puerto: ', this.port);
+        });
+    }
+}
+
+module.exports = Server;
+
+
+
+//io.on('connection', (socket) => {
+    //console.log('cliente conectado!', socket.id);
+    //socket.emit('mensaje-bienvenida', 'Bienvenido al chat');
+/* socket.emit('mensaje-bienvenida', {
+    msg: 'Bienvenido al chat',
+    fecha: new Date().getTime()
+}) */
+
+/* socket.on('mensaje-to-server', (data) => {
+    console.log(data);
+
+    io.emit('mensaje-from-server', data);
+}) */
+//});
